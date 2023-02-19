1130f6196e6e97a712b58869729d4679d46f7b51
Hasna Akbar Ali <akbarali.hasna@gmail.com>
2023-02-19 17:34:43 +0530

    Add files via upload

---
+++ snake_game/snake.py
@@ -0,0 +1,45 @@
+from turtle import Turtle
+
+START_POS = [(0, 0), (-20, 0), (-40, 0)]
+MOVE_DISTANCE = 20
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
+
+class Snake:
+    def __init__(self):
+        self.segments = []
+        self.create_snake()
+        self.head = self.segments[0]
+
+    def create_snake(self):
+        for i in START_POS:
+            new_segment = Turtle('square')
+            new_segment.color('white')
+            new_segment.penup()
+            new_segment.goto(i)
+            self.segments.append(new_segment)
+
+    def move(self):
+        for seg_no in range(len(self.segments) - 1, 0, -1):
+            new_x = self.segments[seg_no - 1].xcor()
+            new_y = self.segments[seg_no - 1].ycor()
+            self.segments[seg_no].goto(new_x, new_y)
+        self.segments[0].forward(MOVE_DISTANCE)
+
+    def go_up(self):
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
+
+    def go_down(self):
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
+
+    def go_right(self):
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
+
+    def go_left(self):
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
