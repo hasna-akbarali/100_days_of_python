c8f4c867950aa1117c64127a20425cb42c081b30
Hasna Akbar Ali <akbarali.hasna@gmail.com>
2023-03-24 08:19:31 +0530

    Add files via upload

---
+++ day_27_miles_to_km_gui/learn.py
@@ -0,0 +1,29 @@
+import tkinter
+#
+# window = tkinter.Tk()
+# window.title('My first GUI Program')
+# window.minsize(width=500,height=300)
+# window.config(padx=10,pady=10)
+#
+# # Label
+# my_label = tkinter.Label(text='I am a Label', font=('Arial', 24))
+# my_label.grid(column=0,row=0)
+#
+# def new_button_clicked():
+#     my_label.config(text=entry.get())
+# # New Button
+# button = tkinter.Button(text='Convert',command=new_button_clicked)
+# button.grid(column=3,row=0)
+#
+# def button_clicked():
+#     my_label.config(text=entry.get())
+# # Button
+# button = tkinter.Button(text='Convert',command=button_clicked)
+# button.grid(column=1,row=1)
+#
+# # Input
+# entry = tkinter.Entry(width=10)
+# entry.grid(column=4,row=4)
+#
+#
+# window.mainloop()
