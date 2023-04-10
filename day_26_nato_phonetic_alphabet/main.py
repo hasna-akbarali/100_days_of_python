051393d0df57e2588a289294f5636b3d66e93c8e
Hasna Akbar Ali <akbarali.hasna@gmail.com>
2023-04-10 13:04:00 +0530

    Update main.py

--- day_26_nato_phonetic_alphabet/main.py
+++ day_26_nato_phonetic_alphabet/main.py
@@ -1,14 +1,20 @@
 import pandas as pd
 
-inp = input('Enter a word: ').upper()
 data = pd.read_csv('nato_phonetic_alphabet.csv')
 
-csv_dict = {row['letter']: row['code'] for (index,row) in data.iterrows()}
-nato_dict = [csv_dict[letter] for letter in inp]
+def natoalphabet():
 
-print(nato_dict)
+    inp = input('Enter a word: ').upper()
+    try:
+        csv_dict = {row['letter']: row['code'] for (index, row) in data.iterrows()}
+        nato_dict = [csv_dict[letter] for letter in inp]
+    except KeyError:
+        print('Sorry, only letters in the alphabet please.')
+    else:
+        print(nato_dict)
 
-
+while True:
+    natoalphabet()
 
 # with open('file1.txt', 'r') as f1:
 #     file1 = f1.readlines()
@@ -19,7 +25,7 @@ print(nato_dict)
 #
 # common = [int(i) for i in file1 if i in file2]
 # print(common)
-
+#
 # sentance = 'What is the Airspeed Velocity of an Unladen Swallow?'
 # words = sentance.split(' ')
 # result = {word:len(word) for word in words}
